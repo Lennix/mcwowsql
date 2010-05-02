e573fea88d229b5874e83b8dd038595c00601f65
Lennix <lennix@mc-wow.de>
2010-05-02 23:02:42 +0200

    Modified fixes

--- Baum/Baum fix 1.10.sql
+++ Baum/Baum fix 1.10.sql
@@ -11,39 +11,16 @@
 -- -------------
 
 -- Totem der Gehirnwäsche
-UPDATE `creature_template` SET `minhealth` = '800',
-`maxhealth` = '800',
-`faction_A` = '37',
-`faction_H` = '37',
-`speed_walk` = '1',
-`speed_run` = '1',
-`unit_flags` = '4',
-`InhabitType` = '1' WHERE `creature_template`.`entry` =15112 LIMIT 1 ;
+UPDATE `creature_template` SET `faction_A` = '37',
+`faction_H` = '37' WHERE `creature_template`.`entry` =15112 LIMIT 1 ;
 
 -- Mächtiger Heiligzauberschutz
-UPDATE `creature_template` SET `minhealth` = '800',
-`maxhealth` = '800',
-`faction_A` = '37',
-`faction_H` = '37',
-`speed_walk` = '1',
-`speed_run` = '1',
-`unit_flags` = '4',
-`InhabitType` = '1' WHERE `creature_template`.`entry` =14987
-LIMIT 1 ;
+UPDATE `creature_template` SET `faction_A` = '37',
+`faction_H` = '37' WHERE `creature_template`.`entry` =14987 LIMIT 1 ;
 
 -- Shade of Jin'do
-UPDATE `creature_template` SET `minlevel` = '60',
-`maxlevel` = '60',
-`minhealth` = '1000',
-`maxhealth` = '1000',
-`minmana` = '500',
-`maxmana` = '500',
-`faction_A` = '37',
-`faction_H` = '37',
-`mindmg` = '0',
-`maxdmg` = '0',
- `rank`  = '1',
-`mechanic_immune_mask` = '0' WHERE `creature_template`.`entry` =14986 LIMIT 1 ;
+UPDATE `creature_template` SET `faction_A` = '37',
+`faction_H` = '37' WHERE `creature_template`.`entry` =14986 LIMIT 1 ;
 
 
 
