d547652b64cfea0ba075905352a88f6dafd55451
Baum <baum@mc-wow.eu>
2010-05-03 20:13:31 +0200

    baumfix 1.06

---
+++ Baum/1.06.sql
@@ -0,0 +1,114 @@
+-------------------------------------------------------
+-- 	MC-Entertainment                               --
+--	Databasefix by Baum UPDATE 1.06 [22.04.2010]   --
+--	for mc-wow [Lennix]                            --
+-------------------------------------------------------
+
+--
+-- Feuerresi alle Feuerimmuner Mobs wird auf 1000 angehoben!
+--
+
+
+-- Onyxia's-Hort
+
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12129 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10184 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11262 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12739 LIMIT 1 ;
+
+-- Molten Core
+
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11658 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11668 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11673 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11669 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12118 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12119 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11671 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11982 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11659 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12259 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11661 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11667 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11666 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12056 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11672 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11988 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12098 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11662 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12018 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11502 LIMIT 1 ;
+
+-- BRD
+
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =8910 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =8909 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =9016 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =9017 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =8911 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =9156 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =8908 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =9938 LIMIT 1 ;
+
+-- UBRS
+
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10366 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =9816 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10372 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10264 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =9096 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10430 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10371 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10814 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10363 LIMIT 1 ;
+
+-- LBRS
+
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10083 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =9568 LIMIT 1 ;
+
+-- BWL
+
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12435 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =13020 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12465 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12464 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12467 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12463 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12468 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12017 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11983 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12461 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =12460 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =14601 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =11981 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =14020 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10162 LIMIT 1 ;
+
+-- SONSTIGE
+
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =7040 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =7048 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =15206 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =7041 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =7045 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =7044 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =10119 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =7043 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =7046 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =7042 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =5852 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =5850 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =8281 LIMIT 1 ;
+UPDATE `creature_template` SET `resistance2` = '1000' WHERE `creature_template`.`entry` =9318 LIMIT 1 ;
+
+
+
+
+
+
+
+
+
+
+
