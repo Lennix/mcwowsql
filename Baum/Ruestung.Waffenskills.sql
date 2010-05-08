714d161b0c8b79448d04179e82825edb0fc752f2
Baum <baum@mc-wow.eu>
2010-05-08 11:20:03 +0200

    Ruestung/Waffenskill INFO

---
+++ Baum/Ruestung.Waffenskills.sql
@@ -0,0 +1,91 @@
+-- ------------------------------------------------------
+-- 	MC-Entertainment                                 --
+--	Waffen- Rüstungskill INFO by Baum [08.05.2010]   --
+--	for mc-wow [Lennix]                              --
+-- ------------------------------------------------------
+
+-- -----------------
+-- RÜSTUNGSKILLS --
+-- -----------------
+
+-- Stoff:
+.learn 9078
+.setskill 415 1 1
+
+-- Leder
+.learn 9077
+.setskill 414 1 1
+
+-- Schwere Rüstung:
+.learn 8737
+.setskill 413 1 1
+
+-- Platte:
+.learn 16320
+.setskill 293 1 1
+
+-- ----------------
+-- WAFFENSKILLS --
+-- ----------------
+
+-- Schild:
+.learn 9116
+.setskill 433 1 1
+
+-- Axt 1-Händig:
+.learn 196
+.setskill 44 300 300
+
+-- Streitkolben 1-Händig
+.learn 198
+.setskill 54 300 300
+
+-- Schwert 1-Händig
+.learn 201
+.setskill 43 300 300
+
+-- Axt 2-Händig:
+.learn 197
+.setskill 172 300 300
+
+-- Streitkolben 2-Händig
+.learn 199
+.setskill 160 300 300
+
+-- Schwert 2-Händig
+.learn 202
+.setskill 55 300 300
+
+-- Dolche
+.learn 1180
+.setskill 173 300 300
+
+-- Bogen:
+.learn 264
+.setskill 45 300 300
+
+-- Schusswaffen:
+.learn 266
+.setskill 46 300 300
+
+-- Armbrust:
+.learn 15995
+.setskill 226 300 300
+
+-- Stab:
+.learn 227
+.setskill136 300 300
+
+-- Zauberstab
+.learn 5009
+.setskill 228 300 300
+
+-- Mit Zauberstab angreifen können
+.learn 5019
+
+-- Wurfwaffen
+.learn 2567
+.setskill 176 300 300
+
+-- Verteidigung
+.setskill 95 300 300
