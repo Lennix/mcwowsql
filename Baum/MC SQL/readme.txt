5e5ffcb90bcc194ed4aa53cc096fb02259d4bcce
Baum <baum@mc-wow.eu>
2010-09-10 18:12:57 +0200

    Update MC sql

---
+++ Baum/MC SQL/readme.txt
@@ -0,0 +1,30 @@
+-runen löschbar
+-majorodomo spawned
+-domo adds (hotfix)
+-lootbox
+-ragnaros event
+-Crashabfrage 100% scripted
+-ragi wird in add phase unsichtbar
+-gehennas adds 
+-garr adds 
+-sulfuron adds
+-sulfuron 100% scripted (need test)
+-geddon workaround
+-garr workaround
+-golemagg workaround
+-major domo workaround
+-lucifron workaround
+-corehoundpacks
+-ragnaros workaround
+-ragi adds scripted
+- magmadar scripted 95%
+-corehound saegebiss fix
+-- 09.09 --
+-sulfuron added seine adds und umgekehrt
+-ragnaros first magmaburst ist getimed
+-shazzrah wirkt seinen fluch auf alle im raid
+-golemag schwächt immer die rüstung(bugt nicht mehr)
+-- 10.09 --
+Lucifron läuft seine richtigen waypoints
+Flammenwichtel respawnen anch 7 Minuten
+
---
+++ Baum/MC SQL/sql immune masc.sql
@@ -0,0 +1,63 @@
+-- -----------------------------
+-- DONE BY SERAFIM AND AZEEM --	überabeitet von Baum
+-- -----------------------------
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11658 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =11668 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =11665 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =12265 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11673 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =12101 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11659 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =11669 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =12119 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =12118 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11671 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11982 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11661 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =12259 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =12099 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073741823' WHERE `creature_template`.`entry` =12057 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =11667 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =11666 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =12076 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073610751' WHERE `creature_template`.`entry` =12100 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =12264 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073741823' WHERE `creature_template`.`entry` =12056 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11662 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =12098 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11672 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11988 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073659903' WHERE `creature_template`.`entry` =11663 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =11664 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073725439' WHERE `creature_template`.`entry` =12018 LIMIT 1 ;
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073741823' WHERE `creature_template`.`entry` =11502 LIMIT 1 ;
